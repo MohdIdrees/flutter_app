421361d93c28fb7f171728ed013b57eb48edf75b
Mohammed Idrees <mohammed.idrees@kelltontech.com>
2022-02-28 15:36:15 +0530

    ++initial commit.

---
+++ lib/utils/routes.dart
@@ -0,0 +1,13 @@
+class AppRoutes {
+  static const loginRoute = "/login";
+  static const signupRoute = "/signup";
+  static const forgotPasswordRoute = "/forgotPassword";
+  static const homeRoute = "/home";
+  static const resultRoute = "/result";
+  static const musicRoute = "/music";
+  static const diceRoute = "/dice";
+  static const destiniRoute = "/destini";
+  static const bmiRoute = "/bmi";
+  static const loadingRoute = "/loading";
+  static const birdRoute = "/bird";
+}
